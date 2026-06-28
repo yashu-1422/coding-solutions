327109eaf444ec5da9b30153ff6722eb163dc305
YASH AUTI <yashauti5@gmail.com>
2026-06-28 18:37:49 +0530

    ✅ Search in Rotated Sorted Array | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.9 MB | 2026-06-28

--- leetcode/medium/0033-search-in-rotated-sorted-array/solution.java
+++ leetcode/medium/0033-search-in-rotated-sorted-array/solution.java
@@ -6,23 +6,19 @@ class Solution {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) {
+            if (nums[mid] == target)
                 return mid;
-            }
 
             // Left half is sorted
             if (nums[left] <= nums[mid]) {
-
                 if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-
             }
             // Right half is sorted
             else {
-
                 if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {

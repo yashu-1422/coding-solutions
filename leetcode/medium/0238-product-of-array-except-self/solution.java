54f3fd48464fb9210a1736e052d810252a3c9f18
YASH AUTI <yashauti5@gmail.com>
2026-06-29 22:47:11 +0530

    ✅ Product of Array Except Self | Medium | Java | Runtime: 4 ms (6.68%) | Memory: 64.9 MB | 2026-06-29

--- leetcode/medium/0238-product-of-array-except-self/solution.java
+++ leetcode/medium/0238-product-of-array-except-self/solution.java
@@ -1,19 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+
         int n = nums.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
         int[] ans = new int[n];
 
-        // Step 1: Store prefix products in ans[]
-        ans[0] = 1;
-        for (int i = 1; i < n; i++) {
-            ans[i] = ans[i - 1] * nums[i - 1];
+        left[0] = 1;
+
+        for(int i=1;i<n;i++){
+            left[i] = left[i-1] * nums[i-1];
         }
 
-        // Step 2: Multiply by suffix products
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] = ans[i] * rightProduct;
-            rightProduct *= nums[i];
+        right[n-1]=1;
+
+        for(int i=n-2;i>=0;i--){
+            right[i]=right[i+1]*nums[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            ans[i]=left[i]*right[i];
         }
 
         return ans;

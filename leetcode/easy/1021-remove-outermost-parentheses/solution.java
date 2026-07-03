b5bd8698e9416f73787194bb247cb41d3e5eeed2
YASH AUTI <yashauti5@gmail.com>
2026-07-03 21:10:07 +0530

    ✅ Remove Outermost Parentheses | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-07-03

--- leetcode/easy/1021-remove-outermost-parentheses/solution.java
+++ leetcode/easy/1021-remove-outermost-parentheses/solution.java
@@ -1,22 +1,24 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        String ans = "";
-        int count = 0;
+        StringBuilder ans = new StringBuilder();
+        int level = 0;
 
-        for (char ch : s.toCharArray()) {
-            if(ch == '(' ){
-                count++;
-                if(count>1){
-                    ans+='(';
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                level++;
+                if (level > 1) {
+                    ans.append(ch);
                 }
-            }
-            else{
-                count--;
-                if(count>0){
-                    ans+=')';
+            } else {
+                level--;
+                if (level > 0) {
+                    ans.append(ch);
                 }
             }
         }
-        return ans ;
+
+        return ans.toString();
     }
 }

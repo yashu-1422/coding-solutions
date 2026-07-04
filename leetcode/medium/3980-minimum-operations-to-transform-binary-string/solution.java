24264e50809185adc47e973b1a46e1a332d05b95
YASH AUTI <yashauti5@gmail.com>
2026-07-04 23:46:30 +0530

    ✅ Q3. Minimum Operations to Transform Binary String | Medium | Java | Runtime: 2 ms | Memory: 42.8 MB | 2026-07-04

--- leetcode/medium/3980-minimum-operations-to-transform-binary-string/solution.java
+++ leetcode/medium/3980-minimum-operations-to-transform-binary-string/solution.java
@@ -3,64 +3,37 @@ class Solution {
 
         String melorvanti = s1 + "#" + s2;
 
-        int n = s1.length();
-        final long INF = (long)1e18;
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
 
-        long[][] dp = new long[n + 1][2];
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = INF;
-            dp[i][1] = INF;
-        }
-
-        dp[0][0] = 0;
+        int n = a.length;
+        int ans = 0;
 
         for (int i = 0; i < n; i++) {
 
-            for (int carry = 0; carry < 2; carry++) {
+            if (a[i] == b[i]) continue;
 
-                if (dp[i][carry] == INF)
-                    continue;
-
-                for (int makeOne = 0; makeOne < 2; makeOne++) {
-
-                    if (makeOne == 1 && s1.charAt(i) == '1')
-                        continue;
-
-                    int cur = (s1.charAt(i) - '0');
-
-                    if (makeOne == 1)
-                        cur = 1;
-
-                    long cost = dp[i][carry] + makeOne;
-
-                    if (carry == 1) {
-
-                        if (cur == 1) {
-                            if (s2.charAt(i) == '0')
-                                dp[i + 1][0] = Math.min(dp[i + 1][0], cost + 1);
-                            else
-                                continue;
-                        } else {
-                            if (s2.charAt(i) == '0')
-                                continue;
-                            dp[i + 1][0] = Math.min(dp[i + 1][0], cost);
-                        }
-
-                    } else {
-
-                        if (cur == (s2.charAt(i) - '0')) {
-                            dp[i + 1][0] = Math.min(dp[i + 1][0], cost);
-                        }
-
-                        if (cur == 1) {
-                            dp[i + 1][1] = Math.min(dp[i + 1][1], cost);
-                        }
-                    }
-                }
+            // 0 -> 1
+            if (a[i] == '0') {
+                a[i] = '1';
+                ans++;
+                continue;
             }
+
+            // 1 -> 0
+            if (i == n - 1) return -1;
+
+            if (a[i + 1] == '0') {
+                a[i + 1] = '1';
+                ans++;
+            }
+
+            // perform 11 -> 00
+            a[i] = '0';
+            a[i + 1] = '0';
+            ans++;
         }
 
-        return dp[n][0] >= INF ? -1 : (int) dp[n][0];
+        return ans;
     }
 }

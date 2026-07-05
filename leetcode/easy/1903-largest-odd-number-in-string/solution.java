5823945db547f0c4179485020b0db3b5e9e61af4
YASH AUTI <yashauti5@gmail.com>
2026-07-05 23:29:40 +0530

    ✅ Largest Odd Number in String | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-07-05

--- leetcode/easy/1903-largest-odd-number-in-string/solution.java
+++ leetcode/easy/1903-largest-odd-number-in-string/solution.java
@@ -4,7 +4,8 @@ class Solution {
         for (int i = num.length() - 1; i >= 0; i--) {
             char ch = num.charAt(i);
 
-            if ((ch - '0') % 2 == 1) {
+            if (ch == '1' || ch == '3' || ch == '5' ||
+                ch == '7' || ch == '9') {
                 return num.substring(0, i + 1);
             }
         }

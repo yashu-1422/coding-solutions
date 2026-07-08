28aa14d60a515b885aa8911c993c4f9c586bd4d3
YASH AUTI <yashauti5@gmail.com>
2026-07-08 20:54:04 +0530

    ✅ Rotate String | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-07-08

--- leetcode/easy/0796-rotate-string/solution.java
+++ leetcode/easy/0796-rotate-string/solution.java
@@ -1,19 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if (s.length() != goal.length()) {
-            return false;
-        }
-
-        // Try all possible rotations of s
-        for (int i = 0; i < s.length(); i++) {
-            String rotated = s.substring(i) + s.substring(0, i);
-            if (rotated.equals(goal)) {
-                // Return true if a match is found
-                return true;  
-            }
-        }
-
+         if (s.length() != goal.length())
         return false;
+
+    return (s + s).contains(goal);
     }
 }
-    

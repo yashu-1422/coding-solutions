64e27439a758fce91aa851dab2d5c27f21ebcb21
YASH AUTI <yashauti5@gmail.com>
2026-07-08 19:10:15 +0530

    ✅ Isomorphic Strings | Easy | Java | Runtime: 6 ms (89.14%) | Memory: 43.8 MB | 2026-07-08

--- leetcode/easy/0205-isomorphic-strings/solution.java
+++ leetcode/easy/0205-isomorphic-strings/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-         int[] m1 = new int[256], m2 = new int[256];
-          int n = s.length();
-          for (int i = 0; i < n; ++i) {
-              // Return false if mapping is inconsistent
-              if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
-  
-              // Update last seen index for both characters
-              m1[s.charAt(i)] = i + 1;
-              m2[t.charAt(i)] = i + 1;
-          }
-  
-          // Return true if all character mappings are consistent
-          return true;
-      }
+        int[] m1 = new int[256], m2 = new int[256];
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            // Return false if mapping is inconsistent
+            if (m1[s.charAt(i)] != m2[t.charAt(i)])
+                return false;
+
+            // Update last seen index for both characters
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+
+        // Return true if all character mappings are consistent
+        return true;
     }
+}

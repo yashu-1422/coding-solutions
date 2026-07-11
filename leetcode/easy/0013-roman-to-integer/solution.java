97652c000761a3f4002332f50e6d72b40686e471
YASH AUTI <yashauti5@gmail.com>
2026-07-11 19:07:07 +0530

    ✅ Roman to Integer | Easy | Java | Runtime: 4 ms (55.47%) | Memory: 46.7 MB | 2026-07-11

---
+++ leetcode/easy/0013-roman-to-integer/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+
+            int curr = map.get(s.charAt(i));
+            int next = map.get(s.charAt(i + 1));
+
+            if (curr < next)
+                ans -= curr;
+            else
+                ans += curr;
+        }
+
+        ans += map.get(s.charAt(s.length() - 1));
+
+        return ans;
+    }
+}

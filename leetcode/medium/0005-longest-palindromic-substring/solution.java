1b9cf979f49fcfb0d6129cc3de0299a4436c54b5
YASH AUTI <yashauti5@gmail.com>
2026-07-20 07:32:01 +0530

    ✅ Longest Palindromic Substring | Medium | Java | Runtime: 14 ms (90.19%) | Memory: 43.4 MB | 2026-07-20

---
+++ leetcode/medium/0005-longest-palindromic-substring/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public String longestPalindrome(String s) {
+
+        if (s == null || s.length() < 2)
+            return s;
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int len1 = expand(s, i, i);       // odd
+            int len2 = expand(s, i, i + 1);   // even
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right) {
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
